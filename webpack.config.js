689f49e7de2f3648b85b2ca962dc7727fa936b6b
dual.rocks Dev <dualrockscontact@gmail.com>
2020-01-25 00:09:37 -0300

    Init dual.rocks ui

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: 'dual.rocks.js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'dual.rocks.css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  }
+};
