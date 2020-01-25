fe8ddf8dae3d2c1c0f99fc6ccef5fa5cd02732b8
dual.rocks Dev <dualrockscontact@gmail.com>
2020-01-25 01:58:38 -0300

    WEB: add login and logout views

--- dual_rocks/web/urls.py
+++ dual_rocks/web/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import (
     HomeView,
+    LoginView,
+    LogoutView,
 )
 
 app_name = 'web'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- dual_rocks/web/views.py
+++ dual_rocks/web/views.py
@@ -1,5 +1,18 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.views import (
+    LoginView as DjangoLoginView,
+    LogoutView as DjangoLogoutView,
+)
 
 
 class HomeView(TemplateView):
     template_name = 'web/home.html'
+
+
+class LoginView(DjangoLoginView):
+    template_name = 'web/login.html'
+    next = 'web:home'
+
+
+class LogoutView(DjangoLogoutView):
+    pass
