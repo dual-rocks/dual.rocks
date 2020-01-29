b1e6ef81cdad7135b78c27c8cbb5fa385cb0bb86
dual.rocks Dev <dualrockscontact@gmail.com>
2020-01-28 23:23:33 -0300

    Add User.at forbidden values valitdator

--- dual_rocks/authentication/models.py
+++ dual_rocks/authentication/models.py
@@ -1,4 +1,5 @@
 from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth.base_user import (
     BaseUserManager,
@@ -7,6 +8,23 @@ from django.contrib.auth.base_user import (
 from django.contrib.auth.models import PermissionsMixin
 from django.core import validators
 from django.urls import reverse
+
+
+class ForbiddenValuesValidator:
+    message = _('"%(value)s" é um valor proibido.')
+    forbidden_values = None
+
+    def __init__(self, forbidden_values=None, message=None):
+        self.message = message or self.message
+        assert forbidden_values
+        self.forbidden_values = forbidden_values
+
+    def __call__(self, value):
+        if value in self.forbidden_values:
+            raise ValidationError(
+                self.message,
+                params={'value': value}
+            )
 
 
 class UserManager(BaseUserManager):
@@ -37,6 +55,14 @@ class User(AbstractBaseUser, PermissionsMixin):
                 r'^[\w\.\_\-]{3,16}$',
                 _('Utilize apenas letras (A-Z), . (ponto), _ (underline) e/ou '
                   '- (hífen). Com no mínimo 3 caracteres e no máximo 16.')
+            ),
+            ForbiddenValuesValidator(
+                [
+                    'login',
+                    'logout',
+                    'register',
+                ],
+                _('Você não pode usar "%(value)s" como @.')
             ),
         ],
         max_length=16
