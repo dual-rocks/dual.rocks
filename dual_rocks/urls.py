b589872d7c540fa2174000562591357ddbc5a390
dual.rocks Dev <dualrockscontact@gmail.com>
2020-01-28 23:44:30 -0300

    Fix admin url and dotted user profile url

--- dual_rocks/urls.py
+++ dual_rocks/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from .web import urls as web_urls
 
 urlpatterns = [
-    path('', include(web_urls, namespace='web')),
     path('admin/', admin.site.urls),
+    path('', include(web_urls, namespace='web')),
 ]
--- dual_rocks/web/urls.py
+++ dual_rocks/web/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('<slug:at>/', profile_view_resolver, name='profile')
+    path('<str:at>/', profile_view_resolver, name='profile')
 ]
