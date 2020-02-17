52135379053427f9002c5fe0d3d9763de96ab5b0
dual.rocks Dev <dualrockscontact@gmail.com>
2020-02-17 20:47:11 -0300

    Add forms.crop_image_from_data fn

---
+++ dual_rocks/forms.py
@@ -0,0 +1,30 @@
+import json
+from PIL import Image
+from io import BytesIO
+
+
+def crop_image_from_data(form, field, crop_data_field):
+    value = form.cleaned_data.get(field)
+    if not value:
+        return value
+    crop_data = json.loads(form.data.get(crop_data_field, '{}'))
+    x = crop_data.get('x')
+    y = crop_data.get('y')
+    width = crop_data.get('width')
+    height = crop_data.get('height')
+    if x is not None \
+        and y is not None \
+            and width is not None \
+            and height is not None:
+        image = Image.open(value.file).crop(
+            (
+                x,
+                y,
+                x + width,
+                y + height
+            )
+        )
+        fmt = value.image.format.lower()
+        value.file = BytesIO()
+        image.save(value.file, fmt)
+    return value
