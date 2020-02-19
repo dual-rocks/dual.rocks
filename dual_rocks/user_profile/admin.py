af7e6df7c1dc34485d8d54a8f589cae80d0257dc
dual.rocks Dev <dualrockscontact@gmail.com>
2020-02-19 01:38:35 -0300

    Add Photo and UserViewPhoto in admin

--- dual_rocks/user_profile/admin.py
+++ dual_rocks/user_profile/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from .models import Profile
+from .models import (
+    Profile,
+    Photo,
+    UserViewPhoto,
+)
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(UserViewPhoto)
+class UserViewPhotoAdmin(admin.ModelAdmin):
     pass
