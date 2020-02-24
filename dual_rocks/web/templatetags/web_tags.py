91014e01f41f5ea2bfd9d76293cbd0af80593b70
dual.rocks Dev <dualrockscontact@gmail.com>
2020-02-23 22:23:50 -0300

    Add get_picture_url template tag in web_tags

--- dual_rocks/web/templatetags/web_tags.py
+++ dual_rocks/web/templatetags/web_tags.py
@@ -8,3 +8,8 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def get_user_view_photo(context, photo):
     return UserViewPhoto.get_or_create(context.request.user, photo)
+
+
+@register.simple_tag(takes_context=True)
+def get_picture_url(context, profile):
+    return profile.get_picture_url(user=context.request.user)
