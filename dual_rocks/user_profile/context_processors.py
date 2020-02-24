a4ec3dd3250c69363f80c3b5b353140771d4f33c
dual.rocks Dev <dualrockscontact@gmail.com>
2020-02-24 10:07:32 -0300

    current_profile ever available in template contex processors

--- dual_rocks/user_profile/context_processors.py
+++ dual_rocks/user_profile/context_processors.py
@@ -1,6 +1,4 @@
 def current_profile(request):
-    if hasattr(request, 'current_profile'):
-        return {
-            'current_profile': request.current_profile
-        }
-    return {}
+    return {
+        'current_profile': request.current_profile
+    }
